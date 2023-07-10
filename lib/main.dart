4199e972a9253e20b76c0b7d4737ebec4cc9bf0f
sajal <sajalsahu8@gmail.com>
2023-07-10 04:54:49 +0400

    displayed todo on the main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:todo/uiupdate.dart';
+import 'package:todo/keys/keys.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: UiUpdatesDemo(),
+          child: Keys(),
         ),
       ),
     ),
