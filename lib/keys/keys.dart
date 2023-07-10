13670eaf1220180552f1457ab844dc5f2a079ef2
sajal <sajalsahu8@gmail.com>
2023-07-10 05:20:43 +0400

    test

--- lib/keys/keys.dart
+++ lib/keys/keys.dart
@@ -71,7 +71,7 @@ class _KeysState extends State<Keys> {
 
               for (final todo in _orderedTodos)
                 CheckableTodoItem(
-                  key: ValueKey(todo.text),
+                  key: ValueKey(todo),
                   //TodoItem(
                   todo.text,
                   todo.priority,
