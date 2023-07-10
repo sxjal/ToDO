505c20cd31fd687ff0ee2fb0b66c0134cbd22fe4
sajal <sajalsahu8@gmail.com>
2023-07-10 04:26:34 +0400

    added a few more widgets

--- lib/uiupdate.dart
+++ lib/uiupdate.dart
@@ -12,6 +12,53 @@ class _UiUpdatesDemoState extends State<UiUpdatesDemo> {
 
   @override
   Widget build(context) {
-    return Text("sajal");
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Every flutter developer should have a basic understanding of rendering of widgets?',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _counter = !_counter;
+                  });
+                },
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 20),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _counter = !_counter;
+                  });
+                },
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+          if (_counter)
+            const Text(
+              'YES!',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
